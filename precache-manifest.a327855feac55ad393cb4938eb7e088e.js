2715c0cc665f20d4d9922a2f38a6df35a1d423af
Khizhniak <ykhizhnyak@gmail.com>
2020-10-28 21:52:08 +0200

    Updates

---
+++ precache-manifest.a327855feac55ad393cb4938eb7e088e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4cb47b6227283b2660f045d1ff38134",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "bf7727bd9692ec9e5587",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.bbbcdd16.chunk.css"
+  },
+  {
+    "revision": "a599f99e53d458bcfa7b",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.8f0014ea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.8f0014ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf7727bd9692ec9e5587",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.fd5082c7.chunk.js"
+  },
+  {
+    "revision": "65c8a2f1bf8ffcc01bde",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.d8bf734b.js"
+  }
+]);
